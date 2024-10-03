f9d39b6960385c6c8fdc6692717849ef869b1178
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2024-10-03 12:58:08 +0200

    Create Scotter.cs

---
+++ Scotter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Scotter : Vehicle
+    {
+        //constant string as TypeOfVehicle wont change allong PoliceCar instances.
+        private static string typeOfVehicle = "Scotter";
+
+        public Scotter() : base(typeOfVehicle) { }
+    }
+}
